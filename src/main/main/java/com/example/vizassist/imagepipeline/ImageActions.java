6ed95205a40eabc60ffe79c5a56a627401ed4246
zsxing99 <44153625+zsxing99@users.noreply.github.com>
2019-11-01 11:33:01 -0700

    Add files via upload

--- src/main/main/java/com/example/vizassist/imagepipeline/ImageActions.java
+++ src/main/main/java/com/example/vizassist/imagepipeline/ImageActions.java
@@ -3,12 +3,39 @@ package com.example.vizassist.imagepipeline;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaSession2Service;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import android.content.Context;
+
+import androidx.core.content.FileProvider;
 
 /**
  * Actions to get images from either the device back camera or photo gallery
  */
 public class ImageActions {
+    public static String currentPhotoPath;
+
+    /**
+     * Create a image file
+     */
+    private static File createImageFile(Context context) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = new File(storageDir,
+                imageFileName +
+                ".jpg"
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
     /**
      * Start the built-in back camera to capture a still image.
@@ -17,7 +44,22 @@ public class ImageActions {
      */
     public static void startCameraActivity(Activity activity, int requestCode) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        activity.startActivityForResult(intent, requestCode);
+        if (intent.resolveActivity(activity.getApplicationContext().getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile(activity);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(activity.getApplicationContext(),
+                        "com.example.vizassist.fileprovider",
+                        photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                activity.startActivityForResult(intent, requestCode);
+            }
+        }
     }
 
     /**
